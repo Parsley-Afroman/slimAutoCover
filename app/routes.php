4c1cf1708203f85554bbdd65e6ada7390a63a471
Sami Shanagher <sshan90@hotmail.com>
2022-04-01 14:50:26 +0100

    added in commentary on what we need to do and started newQuoteController

--- app/routes.php
+++ app/routes.php
@@ -9,4 +9,6 @@ return function (App $app) {
     $app->get('/', 'HomePageController');
     $app->post('/newQuote', 'NewQuoteController');
 
+//    newQuote -> receives the form POST data -> outputs to accept.phtml where you grab $args
+
 };
--- src/Controllers/NewQuoteController.php
+++ src/Controllers/NewQuoteController.php
@@ -6,7 +6,20 @@ class NewQuoteController
 {
     private $carType;
     private $coverType;
+    private $carValue;
+
 //    Create a Viewhelper and get the POST data in order to do the calculation to output (Viewhelper goes in this controller?)
 
+    public function __construct($carType, $coverType, $carValue)
+    {
+        $this->carType = $carType;
+        $this->coverType = $coverType;
+        $this->carValue = $carValue;
+    }
+
+    public function __invoke()
+    {
+        if($carType)
+    }
 
 }
